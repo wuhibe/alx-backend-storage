37658c2a8b3cd87f4eae3cda53617b4913cbcb33
wuhibe <wuhibetamire710@gmail.com>
2022-04-23 21:45:13 +0300

    task 0

--- 0x00-MySQL_Advanced/0-uniq_users.sql
+++ 0x00-MySQL_Advanced/0-uniq_users.sql
@@ -1,6 +1,6 @@
 -- script that creates a table users
 CREATE TABLE IF NOT EXISTS users(
     id INT NOT NULL PRIMARY KEY IDENTITY,
-    email VARCHAR(255),
+    email VARCHAR(255) NOT NULL UNIQUE,
     name VARCHAR(255)
 );
