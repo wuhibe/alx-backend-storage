dac4722427dc4fb5a58fd98a7b5738bd9cb5759a
wuhibe <wuhibetamire710@gmail.com>
2022-04-23 22:40:46 +0300

    task 3

--- 0x00-MySQL_Advanced/3-glam_rock.sql
+++ 0x00-MySQL_Advanced/3-glam_rock.sql
@@ -1,4 +1,5 @@
 -- 3. Old school band
 -- script that lists all bands with Glam rock as their main style
-SELECT `band_name`, IFNULL(`split`, 2022) - `formed` as `lifespan`
-FROM `metal_bands` WHERE FIND_IN_SET('Glam rock', style) ORDER BY `lifespan` DESC;
+SELECT DISTINCT `band_name`,
+                IFNULL(`split`, 2022) - `formed` as `lifespan`
+    FROM `metal_bands` WHERE FIND_IN_SET('Glam rock', style) ORDER BY `lifespan` DESC;
