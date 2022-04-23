a095ce9b09d979b46b69b4a75f8a2eccb5d9c3fa
wuhibe <wuhibetamire710@gmail.com>
2022-04-23 22:53:22 +0300

    NEW.num

--- 0x00-MySQL_Advanced/4-store.sql
+++ 0x00-MySQL_Advanced/4-store.sql
@@ -2,5 +2,5 @@
 -- script that creates a trigger that decr the qty of an item after adding a new order
 CREATE TRIGGER decr_count BEFORE INSERT ON orders
 FOR EACH ROW
-    UPDATE items SET quantity = quantity - 1
+    UPDATE items SET quantity = quantity - NEW.number
         WHERE name = NEW.item_name;
