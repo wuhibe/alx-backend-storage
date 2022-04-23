e0840f552ee9d7034601cdf76356701c6a3b3780
wuhibe <wuhibetamire710@gmail.com>
2022-04-23 23:45:05 +0300

    task 5

--- 0x00-MySQL_Advanced/5-valid_email.sql
+++ 0x00-MySQL_Advanced/5-valid_email.sql
@@ -3,7 +3,7 @@
 CREATE TRIGGER reset_validation BEFORE UPDATE ON users
 FOR EACH ROW
 BEGIN
-    IF NEW.email <> OLD.email THEN
+    IF NEW.email != OLD.email THEN
     SET NEW.valid_email = 0;
     END IF;
 END;
