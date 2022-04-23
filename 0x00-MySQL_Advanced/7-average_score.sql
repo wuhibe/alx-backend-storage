c3233e0a9f52853b2779e8f515d2f867cf673298
wuhibe <wuhibetamire710@gmail.com>
2022-04-24 02:27:50 +0300

    Advanced SQL

--- 0x00-MySQL_Advanced/7-average_score.sql
+++ 0x00-MySQL_Advanced/7-average_score.sql
@@ -7,9 +7,8 @@ CREATE PROCEDURE ComputeOverallScoreForUser (
 )
 BEGIN
     UPDATE users
-    SET average_score = (
-        SELECT AVG(score) FROM corrections
-        WHERE corrections.user_id = user_id)
+    SET
+    average_score = (SELECT AVG(score) FROM corrections WHERE corrections.user_id = user_id)
     WHERE id = user_id;
 END;
 |
