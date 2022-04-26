6835d70961c7b37d5b01bc9ab3c6bb2ccbd7f144
wuhibe <wuhibetamire710@gmail.com>
2022-04-26 20:43:05 +0300

    return find()

---
+++ 0x01-NoSQL/8-all.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python3
+''' module for task 8 '''
+
+
+def list_all(mongo_collection):
+    ''' function that lists all documents in a collection '''
+    return mongo_collection.find()
