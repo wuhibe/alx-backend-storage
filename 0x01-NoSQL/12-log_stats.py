2ae835267bac919184d17d32176c2308a0aca0f5
wuhibe <wuhibetamire710@gmail.com>
2022-04-26 22:33:30 +0300

    logs

---
+++ 0x01-NoSQL/12-log_stats.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python3
+''' module for task 12 '''
+from pymongo import MongoClient
+
+
+def main():
+    client = MongoClient('mongodb://127.0.0.1:27017')
+    lst = client.logs.nginx
+
+    print("{} logs\nMethods:".format(lst.estimated_document_count()))
+
+    for method in ["GET", "POST", "PUT", "PATCH", "DELETE"]:
+        print("\tmethod {}: {}".format(method,
+              lst.count_documents({'method': method})))
+
+    print("{} status check".format(lst.count_documents(
+        {'method': 'GET', 'path': "/status"})))
+
+
+if __name__ == "__main__":
+    main()
