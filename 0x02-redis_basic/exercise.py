8a688ab10132e451fc73819eaf10366d7e5fcbe3
wuhibe <wuhibetamire710@gmail.com>
2022-04-28 04:10:40 +0300

    typing and getters

--- 0x02-redis_basic/exercise.py
+++ 0x02-redis_basic/exercise.py
@@ -6,20 +6,30 @@ import uuid
 
 
 class Cache:
+    ''' Cache class '''
     def __init__(self):
         ''' initialize Class '''
         self._redis = redis.Redis()
         self._redis.flushdb()
 
-    def store(self, data: typing.Any) -> str:
+    def store(self, data: typing.Union[bytes, int, str, float]) -> str:
         ''' method that stores key-value pairs '''
         id: str = str(uuid.uuid4())
         self._redis.mset({id: data})
         return id
 
-    def get(self, key, fn: typing.Callable = None) -> typing.Any:
+    def get(self, key, fn: typing.Optional[typing.Callable]
+            = None) -> typing.Union[bytes, int, str, float]:
         ''' getter method for stored pairs '''
         val: bytes = self._redis.get(key)
         if fn is not None:
             val = fn(val)
         return val
+
+    def get_str(self, key: str) -> str:
+        ''' getter for str '''
+        return self._redis.get(key, str)
+
+    def get_int(self, key: str) -> int:
+        ''' getter for int '''
+        return self._redis.get(key, int)
